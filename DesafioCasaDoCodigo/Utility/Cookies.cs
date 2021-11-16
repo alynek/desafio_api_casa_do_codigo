3e09bfcd446b6f571f3f9ac85aa1380003f165fa
Alyne Gois <alyne0x67@gmail.com>
2021-11-15 22:33:26 -0300

    Implementa classe Cookies que guardará a lógica de serializar os itens do carrinho e gaurdar no cookie

---
+++ DesafioCasaDoCodigo/Utility/Cookies.cs
@@ -0,0 +1,21 @@
+﻿using DesafioCasaDoCodigo.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace DesafioCasaDoCodigo.Utility
+{
+    public class Cookies
+    {
+        public void JsonSerialize(string cookieName, HttpResponse response, Carrinho carrinho)
+        {
+            var cookie = System.Text.Json.JsonSerializer.Serialize(carrinho.livros);
+
+            var options = new CookieOptions()
+            {
+                Path = "/",
+                HttpOnly = true
+            };
+
+            response.Cookies.Append(cookieName, cookie, options);
+        }
+    }
+}
