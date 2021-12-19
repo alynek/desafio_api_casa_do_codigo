c69f50cb2520dfbca99762fc4154a6fbc4ee3532
Alyne Gois <alyne0x67@gmail.com>
2021-12-19 11:05:27 -0300

    Cria DTO para os dados do comprador

---
+++ DesafioCasaDoCodigo/Dtos/DadosCompradorDto.cs
@@ -0,0 +1,21 @@
+﻿using DesafioCasaDoCodigo.Utility;
+using System.ComponentModel.DataAnnotations;
+
+namespace DesafioCasaDoCodigo.Dtos
+{
+    public class DadosCompradorDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Nome { get; set; }
+
+        [Required]
+        [CpfCnpj(ErrorMessage = "O campo Documento é inválido")]
+        public string Documento { get; set; }
+
+        [Required]
+        public string Endereco { get; set; }
+        public string Complemento { get; set; }
+    }
+}
