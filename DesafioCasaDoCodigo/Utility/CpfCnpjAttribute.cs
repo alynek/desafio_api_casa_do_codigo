9f8c9216a781a64f68cf561037d2030944619eea
Alyne Gois <alyne0x67@gmail.com>
2021-12-19 11:04:47 -0300

    Adiciona pacote Caelum Stella C# para ser usado no custom atribute de validação de cpf e Cnpj

---
+++ DesafioCasaDoCodigo/Utility/CpfCnpjAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Caelum.Stella.CSharp.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace DesafioCasaDoCodigo.Utility
+{
+    public class CpfCnpjAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            string stringValue = value as string;
+
+            CPFValidator cpfValidator = new CPFValidator();
+
+            if (cpfValidator.IsValid(stringValue))
+            {
+                return true;
+            }
+
+            CNPJValidator cnpjValidator = new CNPJValidator();
+
+            if (cnpjValidator.IsValid(stringValue))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
