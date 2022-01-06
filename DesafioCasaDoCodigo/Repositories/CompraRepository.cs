0e2a074a05a132a1c1fecd999f8651acce041073
Alyne Gois <alyne0x67@gmail.com>
2022-01-05 21:48:08 -0300

    Cria classe e interface Repository para salvar a compra no banco

---
+++ DesafioCasaDoCodigo/Repositories/CompraRepository.cs
@@ -0,0 +1,22 @@
+﻿using DesafioCasaDoCodigo.Data;
+using DesafioCasaDoCodigo.Models;
+using DesafioCasaDoCodigo.Repositories.Interfaces;
+
+namespace DesafioCasaDoCodigo.Repositories
+{
+    public class CompraRepository : ICompraRepository
+    {
+        private DesafioContext _context;
+
+        public CompraRepository(DesafioContext context)
+        {
+            _context = context;
+        }
+
+        public void Salva(Compra compra)
+        {
+            _context.Compras.Add(compra);
+            _context.SaveChanges();
+        }
+    }
+}
