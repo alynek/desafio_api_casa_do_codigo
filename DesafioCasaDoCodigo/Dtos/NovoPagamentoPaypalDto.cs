4d4b5a25435ae7a278ce613bc78c1bfe650fa22a
Alyne Gois <alyne0x67@gmail.com>
2022-01-15 17:03:35 -0300

    Adiciona classe e dto de pagamento

--- DesafioCasaDoCodigo/Dtos/NovoPagamentoPaypalDto.cs
+++ DesafioCasaDoCodigo/Dtos/NovoPagamentoPaypalDto.cs
@@ -1,9 +1,11 @@
 ﻿using DesafioCasaDoCodigo.Enums;
+using DesafioCasaDoCodigo.Models;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DesafioCasaDoCodigo.Dtos
 {
-    public class NovaCompraPaypalDto
+    public class NovoPagamentoPaypalDto
     {
         [Required]
         public string IdTransacao { get; set; }
@@ -11,5 +13,9 @@ namespace DesafioCasaDoCodigo.Dtos
         [Required]
         public EPaypalStatus Status { get; set; }
 
+        public PagamentoPaypal NovoPagamento(Compra compraExistente)
+        {
+            return new PagamentoPaypal(IdTransacao, Status, compraExistente);
+        }
     }
 }
---
+++ DesafioCasaDoCodigo/Models/PagamentoPaypal.cs
@@ -0,0 +1,40 @@
+﻿using DesafioCasaDoCodigo.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DesafioCasaDoCodigo.Models
+{
+    public class PagamentoPaypal
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string IdTransacao { get; set; }
+
+        [Required]
+        public EPaypalStatus Status { get; set; }
+
+        [Required]
+        public Compra Compra { get; set; }
+
+        public PagamentoPaypal(){}
+        public PagamentoPaypal(string idTransacao, EPaypalStatus status, Compra compraExistente)
+        {
+            IdTransacao = idTransacao;
+            Status = status;
+            Compra = compraExistente;
+        }
+
+        public bool Sucesso()
+        {
+            return EPaypalStatus.sucesso.Equals(Status);
+        }
+
+        public override string ToString()
+        {
+            return "PagamentoPaypal [id=" + Id + ", idTransacao=" + IdTransacao + ", Status=" + Status + "]";
+        }
+    }
+}
