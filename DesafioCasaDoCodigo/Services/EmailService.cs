7bea04c3308b320b692444ae3531b5e488a7b941
Alyne Gois <alyne0x67@gmail.com>
2022-01-15 17:05:39 -0300

    Cria services para enviar email

---
+++ DesafioCasaDoCodigo/Services/EmailService.cs
@@ -0,0 +1,29 @@
+﻿using DesafioCasaDoCodigo.Models;
+using DesafioCasaDoCodigo.Services.Interfaces;
+using System;
+
+namespace DesafioCasaDoCodigo.Services
+{
+    public class EmailService : IEmailService
+    {
+        public void NotificarCompraParaAdminCdc(PagamentoPaypal novoPagamento)
+        {
+            Console.WriteLine("Mandar email para o admin da casa do código");
+            Console.WriteLine(novoPagamento.Compra);
+            Console.WriteLine(novoPagamento);
+        }
+
+        public void NotificarCompradorComNovaCompra(PagamentoPaypal novoPagamento)
+        {
+            Console.WriteLine("Mandar email para comprador da casa do código");
+            Console.WriteLine(novoPagamento.Compra);
+            Console.WriteLine(novoPagamento);
+        }
+        
+        public void NotificarCompraParaAdminCdcPagamentoFalhou(PagamentoPaypal novoPagamento)
+        {
+            Console.WriteLine("Mandar email para admin da casa do código dizendo que a compra falhou");
+            Console.WriteLine(novoPagamento);
+        }
+    }
+}
---
+++ DesafioCasaDoCodigo/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,13 @@
+﻿using DesafioCasaDoCodigo.Models;
+
+namespace DesafioCasaDoCodigo.Services.Interfaces
+{
+    public interface IEmailService
+    {
+        void NotificarCompraParaAdminCdc(PagamentoPaypal novoPagamento);
+
+        void NotificarCompradorComNovaCompra(PagamentoPaypal novoPagamento);
+
+        void NotificarCompraParaAdminCdcPagamentoFalhou(PagamentoPaypal novoPagamento);
+    }
+}
